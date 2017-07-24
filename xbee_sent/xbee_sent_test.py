f71861eec1628bc501ba4e75177590d4ce1f768c
menoina <1p.color.or.2p.color@gmail.com>
2017-07-24 14:38:56 +0900

    きれいにした

--- xbee_sent/xbee_sent_test.py
+++ xbee_sent/xbee_sent_test.py
@@ -10,16 +10,16 @@ xbee = XBee(ser)
 # Send the string 'Hello World' to the module with MY set to 1
 #xbee.send('at',frame_id='A',command='DH',data='hello World')
 #xbee.tx(dest_addr='\x00\x01', data='Hello World')
-'''
+#xbee.tx(data='Hello World')
+
 xbee.send('at', 
         frame_id='A',
-        temp='23',
-        humi='67',
         options='\x02',
         command='D0',
         parameter='\x02')
-'''
-xbee.send('at', frame_id='A', command='DH')
+
+#xbee.send('at', frame_id='A', command='DH',data = 20)
+
 
 # Wait for and get the response
 print(xbee.wait_read_frame())
